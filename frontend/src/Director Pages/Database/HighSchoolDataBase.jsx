757f55e67620511f7b560bcf68314ba71c7560fd
idilozis <ozisbilgeidil@gmail.com>
2024-12-23 22:03:28 +0300

    Fixed linking in director

--- frontend/src/Director Pages/Database/HighSchoolDataBase.jsx
+++ frontend/src/Director Pages/Database/HighSchoolDataBase.jsx
@@ -101,44 +101,22 @@ const HighSchoolDatabase = () => {
         <h2>Bilkent Information Office System</h2>
         <ul>
           <li>
-            <Link to="/api/coordinator_dashboard" className="sidebar-link">
-              Dashboard
-            </Link>
+            <Link to="/api/director_dashboard/" className="sidebar-link">Dashboard</Link>
           </li>
           <li>
-            <Link to="/api/coordinator_highschool_database" className="sidebar-link">
-              High School Database
-            </Link>
+            <Link to="/api/director_database" className="sidebar-link">High School Database</Link>
           </li>
           <li>
-            <Link to="/api/coordinator_puantaj" className="sidebar-link">
-              Puantaj Page
-            </Link>
+            <Link to="/api/director_puantaj_page" className="sidebar-link">Puantaj Page</Link>
           </li>
           <li>
-            <Link to="/api/coordinator_fair_applications" className="sidebar-link">
-              Fair Applications
-            </Link>
+            <Link to="/api/director_fair_applications" className="sidebar-link">Fair Applications</Link>
           </li>
           <li>
-            <Link to="/api/coordinator_accepted_tours" className="sidebar-link">
-              Tours
-            </Link>
+            <Link to="/api/director_accepted_tours" className="sidebar-link">Tours</Link>
           </li>
           <li>
-            <Link to="/api/coordinator_view_advisor_list" className="sidebar-link">
-              Advisor List
-            </Link>
-          </li>
-          <li>
-            <Link to="/api/coordinator_view_guide_list" className="sidebar-link">
-              Guide List
-            </Link>
-          </li>
-          <li>
-            <Link to="/api/coordinator_view_feedback" className="sidebar-link">
-              View Feedbacks
-            </Link>
+            <Link to="/api/director_feedback" className="sidebar-link">View Feedbacks</Link>
           </li>
         </ul>
         <div className="logout">
